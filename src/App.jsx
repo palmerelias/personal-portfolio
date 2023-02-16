a0e2a9dbe8a827368158082dd60b2ef99f25cc48
Elias Palmer <elias@toddsden.com>
2023-02-15 20:19:53 -0500

    added portfolio card

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './app.css'
+import { AiFillLinkedin } from 'react-icons/ai';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    return (
+      <div className='card'>
+        <div>
+          <img src='./images/IMG_5934.jpg' className='profile-img'/>
+            <text className='rectangle'></text>  
+            <text className='elias-palmer'>Elias Palmer</text>
+            <text className='occupation'>Frontend Developer</text>
+            <text className='website-link' href='#'>eliaspalmer.website</text>
+            <button className='email-btn'><img src='./images/Mail.png' />Email</button>
+            <button className='linked-in-btn'><AiFillLinkedin style={{fontSize: "16px", color: "white"}}/>LinkedIn</button>
+            <text className='about-title'>About</text>
+            <text className='about-content'>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</text>
+            <text className='interests-title'>Interests</text>
+            <text className='interests-content'>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</text>
+            <footer>
+              <div className='icon-position'>
+                <img src='./images/GitHub.png' className='icon'/>
+                <img src='./images/Facebook.png' className='icon'/>
+                <img src='./images/Instagram.png' className='icon' />
+                <img src='./images/twitter.png' className='icon' />
+              </div>
+            </footer>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    )
 }
 
 export default App
